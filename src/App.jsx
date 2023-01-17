83308497fa94d3c2cdd673615f0a88e2aca0f0b5
Sajjat Hossain <Sajjad Sadiq>
2023-01-17 22:47:27 +0600

    update initialize router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,47 @@
-import React, { createContext, useContext, useEffect, useState } from "react";
-import { Route, Routes } from "react-router-dom";
-import Navbar from "./layout/Navbar";
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Main from "./layout/Main";
 import About from "./pages/About";
 import Cart from "./pages/Cart";
 import Home from "./pages/Home";
 import TopRated from "./pages/TopRated";
 
-const PRODUCT_CONTEXT = createContext();
+// React Router Setup
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/top-rated",
+        element: <TopRated />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/page-not-found",
+        element: <div>Page Not Found Router</div>,
+      },
+    ],
+  },
+]);
 
 const App = () => {
-  const [products, setProducts] = useState([]);
-  useEffect(() => {
-    fetch("products.json")
-      .then((res) => res.json())
-      .then((data) => setProducts(data));
-  }, []);
-
-  const value = { products };
-
   return (
     <div style={{ padding: "40px 100px" }}>
-      <PRODUCT_CONTEXT.Provider value={value}>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/top-rated" element={<TopRated />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/cart" element={<Cart />} />
-        </Routes>
-      </PRODUCT_CONTEXT.Provider>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
-};
-
-export const useProducts = () => {
-  const context = useContext(PRODUCT_CONTEXT);
-  return context;
 };
 
 export default App;
